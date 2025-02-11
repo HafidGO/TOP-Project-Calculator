02d610b492ce490992af6e350a0661d573164fcf
HafidGO <106498821+HafidGO@users.noreply.github.com>
2025-02-10 21:35:05 -0600

    Working on operation script

---
+++ script.js
@@ -0,0 +1,20 @@
+// let a = ;
+// let a = ;
+// let a = ;
+
+function add(a,b){
+    return a + b; 
+}
+
+function subtract(a,b){
+    return a - b; 
+}
+
+function multiply(a,b){
+    return a * b; 
+}
+
+function divide(a,b){
+    return a / b; 
+}
+
