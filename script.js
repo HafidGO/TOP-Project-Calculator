279d37c2a0ddd63e8833bffcabe4f224f946e7ba
HafidGO <22013428@es.uveg.edu.mx>
2025-02-11 21:13:30 -0600

    Working on script

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-// let a = ;
-// let a = ;
-// let a = ;
+
+
+//Expect first number
+//Listen for an operation
+//Expect second number
+//After pressing the equal button the operation shall be executed.
+
+let a = ;
+let b = ; 
 
 function add(a,b){
     return a + b; 
